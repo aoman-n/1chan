22f76c78e18cc7a24232b7f1294768b9584eda1e
aoba <ipbdipbd00@gmail.com>
2019-12-10 21:33:42 +0900

    Fix work directory path & Add logging package

--- src/api/main.go
+++ src/api/main.go
@@ -1,31 +1,31 @@
 package main
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
-    _ "github.com/jinzhu/gorm/dialects/mysql"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/laster18/1chan/src/api/utils"
 )
 
 type Product struct {
-    gorm.Model
-    Code  string
-    Price uint
+	gorm.Model
+	Code  string
+	Price uint
 }
 
 func main() {
+	utils.LoggingSettings("1chan.log")
+
 	// TODO: DB情報を環境変数+configで管理する
 	db, err := gorm.Open("mysql", "root:password@tcp(db:3306)/1chan?charset=utf8&parseTime=True&loc=Local")
-    if err != nil {
-        panic("failed to connect database")
-    }
-    defer db.Close()
+	if err != nil {
+		panic("failed to connect database")
+	}
+	defer db.Close()
 
-    db.AutoMigrate(&Product{})
-
+	db.AutoMigrate(&Product{})
 
 	r := gin.Default()
-	fmt.Println("hoge")
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "ping",
