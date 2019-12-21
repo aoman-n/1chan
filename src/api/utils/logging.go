fbc9cb41433ed447812836fd9cc1c1c07bfc9bc4
aoba <ipbdipbd00@gmail.com>
2019-12-21 11:44:53 +0900

    Fix write gin log to file

--- src/api/utils/logging.go
+++ src/api/utils/logging.go
@@ -2,13 +2,19 @@ package utils
 
 import (
 	"io"
-	"log"
 	"os"
+
+	"github.com/gin-gonic/gin"
 )
 
 func LoggingSettings(logFile string) {
-	logfile, _ := os.OpenFile(logFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	multiLogFile := io.MultiWriter(os.Stdout, logfile)
-	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
-	log.SetOutput(multiLogFile)
+	// logfile, _ := os.OpenFile(logFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	// multiLogFile := io.MultiWriter(os.Stdout, logfile)
+	// log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+	// log.SetOutput(multiLogFile)
+	gin.DisableConsoleColor()
+
+	// Logging to a file.
+	f, _ := os.Create(logFile)
+	gin.DefaultWriter = io.MultiWriter(f)
 }
